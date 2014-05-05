1cacac227c3e605d31a4284c9c4b52d1e33178ba
benbeyerlein <benbeyerlein@gmail.com>
2014-05-04 19:27:22 -0500

    Task 3 complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,16 +11,18 @@ class QuestionsController < ApplicationController
     # In what year was the oldest movie on the list released?
 
     # Your Ruby goes here.
-
-    # @year_of_oldest_movie = ???
+    @year_of_oldest_movie = Movie.order("year ASC").first.year
   end
 
   def question_3
     # How many movies does the director of the first movie have on the list?
 
     # Your Ruby goes here.
+    fst_mov_dr = Movie.first.director_id
 
-    # @number_of_movies_directed_by_first_movie_director = ???
+    num_mov_by_dr = Movie.where({:director_id => fst_mov_dr}).count
+
+    @number_of_movies_directed_by_first_movie_director = num_mov_by_dr
   end
 
   def question_4
@@ -29,7 +31,11 @@ class QuestionsController < ApplicationController
     # Your Ruby goes here.
     # You'll probably have to use both ActiveRecord query methods as well as some plain old Ruby logic.
 
-    # @most_number_of_movies_by_a_single_director = ???
+    cnt_by_dir = Movie.group(:director_id).count
+
+    max_cnt_by_dir = cnt_by_dir.values.max
+
+    @most_number_of_movies_by_a_single_director = max_cnt_by_dir
   end
 
   def question_5
@@ -38,6 +44,18 @@ class QuestionsController < ApplicationController
     # Your Ruby goes here.
     # You'll probably have to use both ActiveRecord query methods as well as some plain old Ruby logic.
 
-    # @most_recent_movie_for_first_actor = ???
+    # Create a hash of all the movies actor 1 has been in
+    first_actor = Actor.first.id
+
+    fist_actors_movies = Role.where({:actor_id => first_actor}).pluck(:movie_id)
+
+    # Subset the movie table by the list from actor 1
+    list_of_movies = Movie.find(fist_actors_movies)
+
+    # Get the max year value
+    sorted_list = list_of_movies.sort_by { |k| k["year"]}
+
+
+    @most_recent_movie_for_first_actor = sorted_list.last.title
   end
 end
